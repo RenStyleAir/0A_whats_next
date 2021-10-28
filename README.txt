d91bc80f2f3b08fb16075030ada417b21bd6618f
Ray <rayco020@hotmail.com>
2021-10-28 17:59:37 +0200

    first

---
+++ README.txt
@@ -0,0 +1,22 @@
+Whats next! a game of chance and memory.
+
+Can you get 7 point? 
+
+by RenAir
+
+
+- setup + git hub
+
+- dek of 13 cards, select 8 
+
+- read the cards out (hide card)
+
+- determin answers 
+
+- go trough answers and take input
+
+- get the score
+
++ hard image 
+
++ win screen! 
---
+++ whats_next.py
@@ -0,0 +1,25 @@
+# Whats next! a game of chance and memory.
+# Can you get 7 point? 
+# by RenAir
+
+# - setup + git hub
+# - dek of 13 cards, select 8 
+# - read the cards out (hide card)...
+
+import random
+
+
+dek = []
+for card in range(13):
+    dek.append(card +1)
+
+random_eight = []
+
+for pick in range(8):
+    random_eight.append()
+
+
+print(dek)
+
+
+
